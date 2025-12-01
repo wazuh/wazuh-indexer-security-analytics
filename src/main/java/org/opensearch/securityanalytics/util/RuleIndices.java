37ee1080e0db13b4df985eebf2f8f038aa76c244
QU3B1M <kevin.ledesma@wazuh.com>
2025-12-01 17:59:01 -0300

    Change test env var to a property

--- src/main/java/org/opensearch/securityanalytics/util/RuleIndices.java
+++ src/main/java/org/opensearch/securityanalytics/util/RuleIndices.java
@@ -264,11 +264,12 @@ public class RuleIndices {
         Stream<Path> folder = Files.list(path);
         List<Path> folderPaths = folder.collect(Collectors.toList());
         Map<String, List<String>> logIndexToRules = new HashMap<>();
+        if (System.getProperty("INDEXER_TEST_ENV").toString().equals("true")) {
              for (Path folderPath: folderPaths) {
                  List<String> rules = getRules(List.of(folderPath));
                  String ruleCategory = getRuleCategory(folderPath);
                  logIndexToRules.put(ruleCategory, rules);
-             }
+             }}
         checkLogTypes(logIndexToRules, refreshPolicy, indexTimeout, listener);
     }
 
