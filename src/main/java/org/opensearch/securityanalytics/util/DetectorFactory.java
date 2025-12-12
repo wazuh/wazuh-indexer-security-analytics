4a3d8e131ca751813d70392bbc09e09ba904dc96
QU3B1M <kevin.ledesma@wazuh.com>
2025-12-12 09:01:29 -0300

    Implement integration datastream as detector index

--- src/main/java/org/opensearch/securityanalytics/util/DetectorFactory.java
+++ src/main/java/org/opensearch/securityanalytics/util/DetectorFactory.java
@@ -10,21 +10,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DetectorFactory {
+    public static final String DEFAULT_RULE_INDEX = ".rules_development_0.0.1-rules_development_0.0.1_test-rule";
     /* Creates a Detector object with the given rules, log type, and index name */
     public static Detector createDetector(List<String> rulesIds, String logType, String indexName) {
         List<DetectorRule> detectorRules = new ArrayList<>();
         // Detector body/mapping
         String id = ""; // Empty ID for new detector
-        Long version = 0L; // Initial version
-        String name = logType + "-detector"; // e.g., "cisco-ios-detector"
+        Long version = 0L;
+        String name = logType + "-detector";
         String description = "Detector for " + logType + " integration";
-        List<String> indices = List.of(indexName);
+        String dataStream = "wazuh-events-v5*";
         IntervalSchedule schedule = new IntervalSchedule(1, ChronoUnit.MINUTES, null);
         // Add rules to detector input
         for (String ruleId : rulesIds) {
             detectorRules.add(new DetectorRule(ruleId));
         }
-        DetectorInput detectorInput = new DetectorInput(description, indices, new ArrayList<>(), detectorRules);
+        DetectorInput detectorInput = new DetectorInput(description, List.of(dataStream), detectorRules, new ArrayList<>());
         // Generate Detector object with this template
         return new Detector(
                 id,
@@ -39,7 +40,7 @@ public class DetectorFactory {
                 List.of(detectorInput),
                 new ArrayList<>(),
                 new ArrayList<>(),
-                null,
+                DEFAULT_RULE_INDEX,
                 null,
                 null,
                 null,
