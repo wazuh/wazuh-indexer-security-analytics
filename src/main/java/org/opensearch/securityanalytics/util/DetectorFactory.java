1e2d1512e6c053cfe55f338fb7590b448dd6583b
QU3B1M <kevin.ledesma@wazuh.com>
2025-12-12 08:06:18 -0300

    Set initial Detector mapping

--- src/main/java/org/opensearch/securityanalytics/util/DetectorFactory.java
+++ src/main/java/org/opensearch/securityanalytics/util/DetectorFactory.java
@@ -10,31 +10,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DetectorFactory {
+    /* Creates a Detector object with the given rules, log type, and index name */
     public static Detector createDetector(List<String> rulesIds, String logType, String indexName) {
         List<DetectorRule> detectorRules = new ArrayList<>();
-        List<String> indices = List.of(indexName);
-        // Def
-//        Instant Instant = java.time.Instant.now().minusSeconds(3600);
-        IntervalSchedule schedule = new IntervalSchedule(1, ChronoUnit.MINUTES, null);
+        // Detector body/mapping
+        String id = ""; // Empty ID for new detector
+        Long version = 0L; // Initial version
+        String name = logType + "-detector"; // e.g., "cisco-ios-detector"
         String description = "Detector for " + logType + " integration";
-
+        List<String> indices = List.of(indexName);
+        IntervalSchedule schedule = new IntervalSchedule(1, ChronoUnit.MINUTES, null);
+        // Add rules to detector input
         for (String ruleId : rulesIds) {
             detectorRules.add(new DetectorRule(ruleId));
         }
-
-        List<DetectorInput> inputs = List.of(new DetectorInput(description, indices, new ArrayList<>(), detectorRules));
+        DetectorInput detectorInput = new DetectorInput(description, indices, new ArrayList<>(), detectorRules);
 
         return new Detector(
-                "",
-                0L,
-                "Test detector",
+                id,
+                version,
+                name,
                 true,
                 schedule,
                 java.time.Instant.now(),
                 java.time.Instant.now(),
                 logType,
                 null,
-                inputs,
+                List.of(detectorInput),
                 new ArrayList<>(),
                 new ArrayList<>(),
                 null,
