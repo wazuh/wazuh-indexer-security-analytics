86bcd56451007dd04cdc05b171abd8530a89a41c
Álex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>
2025-12-17 11:56:43 +0100

    Initialize threat detectors (#15)
    Co-authored-by: QU3B1M <kevin.ledesma@wazuh.com>
    Co-authored-by: f-galland <federico.galland@wazuh.com>

---
+++ commons/src/main/java/com/wazuh/securityanalytics/action/WIndexDetectorRequest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wazuh.securityanalytics.action;
+
+import org.opensearch.action.ActionRequest;
+import org.opensearch.action.ActionRequestValidationException;
+import org.opensearch.action.support.WriteRequest;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+
+import java.io.IOException;
+import java.util.List;
+
+public class WIndexDetectorRequest extends ActionRequest {
+    private final String logTypeName;
+    private final String category;
+    private final List<String> rules;
+    private final WriteRequest.RefreshPolicy refreshPolicy;
+
+    public WIndexDetectorRequest(
+            String logTypeName,
+            String category,
+            List<String> rules,
+            WriteRequest.RefreshPolicy refreshPolicy) {
+        super();
+        this.logTypeName = logTypeName;
+        this.category = category;
+        this.rules = rules;
+        this.refreshPolicy = refreshPolicy;
+    }
+
+    public WIndexDetectorRequest(StreamInput sin) throws IOException {
+        this(
+            sin.readString(),
+            sin.readString(),
+            sin.readStringList(),
+            WriteRequest.RefreshPolicy.readFrom(sin)
+        );
+    }
+
+    @Override
+    public ActionRequestValidationException validate() {
+        return null;
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeString(this.logTypeName);
+        out.writeString(this.category);
+        out.writeStringCollection(this.rules);
+        this.refreshPolicy.writeTo(out);
+    }
+
+    public String getLogTypeName() {
+        return this.logTypeName;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public List<String> getRules() {
+        return this.rules;
+    }
+
+    public WriteRequest.RefreshPolicy getRefreshPolicy() {
+        return this.refreshPolicy;
+    }
+}
