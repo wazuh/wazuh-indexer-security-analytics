c53209738226cae8f47259396298041039e7427c
Jorge Sanchez <jorge.sanchez@wazuh.com>
2026-01-08 13:10:56 +0100

    Delete extra comments

--- commons/src/main/java/com/wazuh/securityanalytics/action/WDeleteIntegrationResponse.java
+++ commons/src/main/java/com/wazuh/securityanalytics/action/WDeleteIntegrationResponse.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.wazuh.securityanalytics.action;
 
 import java.io.IOException;
