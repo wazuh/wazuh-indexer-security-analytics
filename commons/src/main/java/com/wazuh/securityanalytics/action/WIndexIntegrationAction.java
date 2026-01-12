d726aeb6a559860f3b105670dc6cebde251a8d1f
Kevin Ledesma <kevin.ledesma@wazuh.com>
2026-01-12 08:06:41 -0300

    Improve SAP code quality (#26)

--- commons/src/main/java/com/wazuh/securityanalytics/action/WIndexIntegrationAction.java
+++ commons/src/main/java/com/wazuh/securityanalytics/action/WIndexIntegrationAction.java
@@ -10,11 +10,26 @@ package com.wazuh.securityanalytics.action;
 
 import org.opensearch.action.ActionType;
 
+/**
+ * Action type for indexing Wazuh integrations (custom log types).
+ * <p>
+ * This action is registered under the cluster admin namespace and provides
+ * the entry point for integration write operations through the transport layer.
+ * <p>
+ * Action name: cluster:admin/wazuh/securityanalytics/logtype/write
+ *
+ * @see WIndexIntegrationRequest
+ * @see WIndexIntegrationResponse
+ */
 public class WIndexIntegrationAction extends ActionType<WIndexIntegrationResponse> {
 
+    /** Singleton instance of this action. */
     public static final WIndexIntegrationAction INSTANCE = new WIndexIntegrationAction();
+
+    /** The action name used for transport registration. */
     public static final String NAME = "cluster:admin/wazuh/securityanalytics/logtype/write";
 
+    /** Private constructor to enforce singleton pattern. */
     public WIndexIntegrationAction() {
         super(NAME, WIndexIntegrationResponse::new);
     }
