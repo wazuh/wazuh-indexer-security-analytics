cf9702adcd31dc8230323b4e1f313c9289792e3b
Fero Jiménez <fero.jimenez@wazuh.com>
2026-04-06 14:21:40 +0200

    fix: detector creation to use document.id + space

--- src/main/java/org/opensearch/securityanalytics/transport/TransportIndexDetectorAction.java
+++ src/main/java/org/opensearch/securityanalytics/transport/TransportIndexDetectorAction.java
@@ -2137,7 +2137,15 @@ public class TransportIndexDetectorAction
                             .map(DetectorRule::getId)
                             .collect(Collectors.toList());
 
-            QueryBuilder queryBuilder = QueryBuilders.termsQuery("_id", ruleIds.toArray(new String[] {}));
+            // Rules are identified by document.id (from the content manager) + space=Custom.
+            // Querying by _id would fail because the IDs received here are document.id values,
+            // not the internal _id of the custom rules index.
+            QueryBuilder queryBuilder = QueryBuilders.nestedQuery(
+                    "rule",
+                    QueryBuilders.boolQuery()
+                            .must(QueryBuilders.termsQuery("rule.document.id", ruleIds.toArray(new String[]{})))
+                            .filter(QueryBuilders.termQuery("rule.space", "Custom")),
+                    ScoreMode.None);
             SearchRequest searchRequest =
                     new SearchRequest(Rule.CUSTOM_RULES_INDEX)
                             .source(
@@ -2147,7 +2155,7 @@ public class TransportIndexDetectorAction
                                             .query(queryBuilder)
                                             .size(10000))
                             .preference(Preference.PRIMARY_FIRST.type());
-            logger.debug("importing custom rules");
+            logger.debug("importing custom rules by document.id with space=Custom");
             client.search(
                     searchRequest,
                     new ActionListener<>() {
@@ -2160,6 +2168,16 @@ public class TransportIndexDetectorAction
                             }
                             logger.debug("custom rules fetch successful");
                             SearchHits hits = response.getHits();
+
+                            if (hits.getHits().length == 0 && !ruleIds.isEmpty()) {
+                                onFailures(new OpenSearchStatusException(
+                                        "Detector cannot be created: no custom rules found for the provided"
+                                                + " document IDs in Custom space. Ensure the rules have been"
+                                                + " promoted to Custom space before creating a detector."
+                                                + " Provided document IDs: " + ruleIds,
+                                        RestStatus.BAD_REQUEST));
+                                return;
+                            }
 
                             try {
                                 for (SearchHit hit : hits) {
