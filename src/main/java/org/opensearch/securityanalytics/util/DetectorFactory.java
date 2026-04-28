756e897c18004e2fa75faf291b55bcb17b10a87a
ana-rm <ana.rejano@wazuh.com>
2026-04-28 10:21:17 +0200

    Add methods to DectectorFactory and WIndexDetectorRequest tests

--- src/main/java/org/opensearch/securityanalytics/util/DetectorFactory.java
+++ src/main/java/org/opensearch/securityanalytics/util/DetectorFactory.java
@@ -99,6 +99,7 @@ public class DetectorFactory {
                 null,
                 null,
                 null,
-                false);
+                false,
+                null);
     }
 }
