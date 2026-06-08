6d0c364ccf4e7081901006b58bc89683e9061574
Fede Galland <federico.galland@wazuh.com>
2026-06-08 10:57:37 -0300

    Remove unused settings (#221)

--- src/main/java/org/opensearch/securityanalytics/settings/SecurityAnalyticsSettings.java
+++ src/main/java/org/opensearch/securityanalytics/settings/SecurityAnalyticsSettings.java
@@ -19,7 +19,6 @@ package org.opensearch.securityanalytics.settings;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.common.unit.TimeValue;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SecurityAnalyticsSettings {
@@ -39,21 +38,14 @@ public class SecurityAnalyticsSettings {
     public static final Setting<Boolean> ALERT_HISTORY_ENABLED =
             Setting.boolSetting(
                     "plugins.security_analytics.alert_history_enabled",
-                    true,
+                    false,
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
 
     public static final Setting<Boolean> FINDING_HISTORY_ENABLED =
             Setting.boolSetting(
                     "plugins.security_analytics.alert_finding_enabled",
-                    true,
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    public static final Setting<Boolean> IOC_FINDING_HISTORY_ENABLED =
-            Setting.boolSetting(
-                    "plugins.security_analytics.ioc_finding_enabled",
-                    true,
+                    false,
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
 
@@ -78,13 +70,6 @@ public class SecurityAnalyticsSettings {
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
 
-    public static final Setting<TimeValue> IOC_FINDING_HISTORY_ROLLOVER_PERIOD =
-            Setting.positiveTimeSetting(
-                    "plugins.security_analytics.ioc_finding_history_rollover_period",
-                    TimeValue.timeValueHours(12),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
     public static final Setting<TimeValue> ALERT_HISTORY_INDEX_MAX_AGE =
             Setting.positiveTimeSetting(
                     "plugins.security_analytics.alert_history_max_age",
@@ -102,13 +87,6 @@ public class SecurityAnalyticsSettings {
     public static final Setting<TimeValue> CORRELATION_HISTORY_INDEX_MAX_AGE =
             Setting.positiveTimeSetting(
                     "plugins.security_analytics.correlation_history_max_age",
-                    new TimeValue(30, TimeUnit.DAYS),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    public static final Setting<TimeValue> IOC_FINDING_HISTORY_INDEX_MAX_AGE =
-            Setting.positiveTimeSetting(
-                    "plugins.security_analytics.ioc_finding_history_max_age",
                     new TimeValue(30, TimeUnit.DAYS),
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
@@ -138,14 +116,6 @@ public class SecurityAnalyticsSettings {
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
 
-    public static final Setting<Long> IOC_FINDING_HISTORY_MAX_DOCS =
-            Setting.longSetting(
-                    "plugins.security_analytics.ioc_finding_history_max_docs",
-                    1000L,
-                    0L,
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
     public static final Setting<TimeValue> ALERT_HISTORY_RETENTION_PERIOD =
             Setting.positiveTimeSetting(
                     "plugins.security_analytics.alert_history_retention_period",
@@ -164,27 +134,6 @@ public class SecurityAnalyticsSettings {
             Setting.positiveTimeSetting(
                     "plugins.security_analytics.correlation_history_retention_period",
                     new TimeValue(60, TimeUnit.DAYS),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    public static final Setting<TimeValue> IOC_FINDING_HISTORY_RETENTION_PERIOD =
-            Setting.positiveTimeSetting(
-                    "plugins.security_analytics.ioc_finding_history_retention_period",
-                    new TimeValue(60, TimeUnit.DAYS),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    public static final Setting<TimeValue> REQUEST_TIMEOUT =
-            Setting.positiveTimeSetting(
-                    "plugins.security_analytics.request_timeout",
-                    TimeValue.timeValueSeconds(10),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    public static final Setting<TimeValue> MAX_ACTION_THROTTLE_VALUE =
-            Setting.positiveTimeSetting(
-                    "plugins.security_analytics.action_throttle_max_value",
-                    TimeValue.timeValueHours(24),
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
 
@@ -224,67 +173,6 @@ public class SecurityAnalyticsSettings {
             Setting.simpleString(
                     "plugins.security_analytics.mappings.default_schema",
                     "ecs",
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    // threat intel settings
-    public static final Setting<TimeValue> TIF_UPDATE_INTERVAL =
-            Setting.timeSetting(
-                    "plugins.security_analytics.threatintel.tifjob.update_interval",
-                    TimeValue.timeValueMinutes(1440),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    /** Bulk size for indexing threat intel feed data */
-    public static final Setting<Integer> BATCH_SIZE =
-            Setting.intSetting(
-                    "plugins.security_analytics.threatintel.tifjob.batch_size",
-                    10000,
-                    1,
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    /** Timeout value for threat intel processor */
-    public static final Setting<TimeValue> THREAT_INTEL_TIMEOUT =
-            Setting.timeSetting(
-                    "plugins.security_analytics.threat_intel_timeout",
-                    TimeValue.timeValueSeconds(30),
-                    TimeValue.timeValueSeconds(1),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    /**
-     * Return all settings of threat intel feature
-     *
-     * @return a list of all settings for threat intel feature
-     */
-    public static final List<Setting<?>> settings() {
-        return List.of(BATCH_SIZE, THREAT_INTEL_TIMEOUT, TIF_UPDATE_INTERVAL);
-    }
-
-    // Threat Intel IOC Settings
-    public static final Setting<TimeValue> IOC_INDEX_RETENTION_PERIOD =
-            Setting.timeSetting(
-                    "plugins.security_analytics.ioc.index_retention_period",
-                    new TimeValue(30, TimeUnit.DAYS),
-                    new TimeValue(1, TimeUnit.DAYS),
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    public static final Setting<Integer> IOC_MAX_INDICES_PER_INDEX_PATTERN =
-            Setting.intSetting(
-                    "plugins.security_analytics.ioc.max_indices_per_alias",
-                    2,
-                    1,
-                    Setting.Property.NodeScope,
-                    Setting.Property.Dynamic);
-
-    /** Maximum terms in Terms query search query submitted during ioc scan */
-    public static final Setting<Integer> IOC_SCAN_MAX_TERMS_COUNT =
-            Setting.intSetting(
-                    "plugins.security_analytics.ioc.scan_max_terms_count",
-                    65536,
-                    1,
                     Setting.Property.NodeScope,
                     Setting.Property.Dynamic);
 
