8f60c36e91ebdbfedb7523cfcd5f86acc08e5333
Jorge Sánchez <jorge.sanchez@wazuh.com>
2026-06-08 16:29:28 +0200

    Change rule field from nested to object (#218)
    Co-authored-by: Álex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>

--- src/main/java/org/opensearch/securityanalytics/transport/TransportIndexRuleAction.java
+++ src/main/java/org/opensearch/securityanalytics/transport/TransportIndexRuleAction.java
@@ -465,12 +465,9 @@ public class TransportIndexRuleAction
                 String documentId,
                 String space) {
             QueryBuilder query =
-                    QueryBuilders.nestedQuery(
-                            "rule",
-                            QueryBuilders.boolQuery()
-                                    .filter(QueryBuilders.termQuery("rule.document.id", documentId))
-                                    .filter(QueryBuilders.termQuery("rule.space", space)),
-                            ScoreMode.None);
+                    QueryBuilders.boolQuery()
+                            .filter(QueryBuilders.termQuery(Rule.RULE + "." + Rule.DOCUMENT_ID_FIELD, documentId))
+                            .filter(QueryBuilders.termQuery(Rule.RULE + "." + Rule.SPACE_FIELD, space));
             SearchRequest searchRequest =
                     new SearchRequest(Rule.CUSTOM_RULES_INDEX)
                             .source(new SearchSourceBuilder().query(query).size(1))
