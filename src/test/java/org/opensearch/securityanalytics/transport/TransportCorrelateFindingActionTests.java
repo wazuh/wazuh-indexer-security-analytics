7cdce8cb332a7edeed191def63836f2b0efb0eab
Jorge Sanchez <jorge.sanchez@wazuh.com>
2026-06-30 15:15:41 +0200

    Fix tests

--- src/test/java/org/opensearch/securityanalytics/transport/TransportCorrelateFindingActionTests.java
+++ src/test/java/org/opensearch/securityanalytics/transport/TransportCorrelateFindingActionTests.java
@@ -157,6 +157,11 @@ public class TransportCorrelateFindingActionTests extends OpenSearchTestCase {
                                         SecurityAnalyticsSettings.ENRICHED_FINDINGS_ENABLED,
                                         SecurityAnalyticsSettings.CORRELATION_DETECTOR_CACHE_TTL,
                                         SecurityAnalyticsSettings.CORRELATION_MAX_IN_FLIGHT_FINDINGS,
+                                        SecurityAnalyticsSettings.CORRELATION_MAX_PENDING_FINDINGS,
+                                        SecurityAnalyticsSettings.EVENTS_BACKPRESSURE_ENABLED,
+                                        SecurityAnalyticsSettings.EVENTS_BACKPRESSURE_HIGH_WATERMARK_PERCENT,
+                                        SecurityAnalyticsSettings.EVENTS_BACKPRESSURE_LOW_WATERMARK_PERCENT,
+                                        SecurityAnalyticsSettings.EVENTS_BACKPRESSURE_INDEX_PATTERN,
                                         SecurityAnalyticsSettings.CORRELATION_METADATA_CACHE_TTL)));
         when(clusterService.getClusterSettings()).thenReturn(clusterSettings);
         when(clusterService.state()).thenReturn(ClusterState.builder(new ClusterName("test")).build());
